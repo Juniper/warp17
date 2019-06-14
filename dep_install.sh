b3ffc050e5a429cbd0339384aebc5e30f35ccdd7
Matteo Triggiani <davvore33@gmail.com>
2019-06-14 11:12:43 +0200

    Add script to istall old deb
    Issue: https://github.com/Juniper/warp17/issues/92

---
+++ dep_install.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+#
+# Copyright (c) 2018, Juniper Networks, Inc. All rights reserved.
+#
+#
+# The contents of this file are subject to the terms of the BSD 3 clause
+# License (the "License"). You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at
+# https://github.com/Juniper/warp17/blob/master/LICENSE.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# File name:
+#     dep_install.sh
+#
+# Description:
+#     Warp17 dep installation for Ubuntu
+#
+# Author:
+#     Matteo Triggiani
+#
+# Initial Created:
+#     06/13/2019
+#
+# Notes:
+#     
+
+# Parse args
+source common/common.sh
+set -e
+
+sudo apt-get install build-essential libnuma-dev python ncurses-dev sudo
+
+# Protobuf2 dependencies
+sudo apt-get install zlib1g-dev
+# ATTENTION: this will go away once we will support Protobuf v3
+dep_file=Dependencies.zip
+workdir=/tmp/deps
+
+mkdir -p $workdir
+cd $workdir
+
+curl -G https://storage.googleapis.com/drive-bulk-export-anonymous/20190614T085116Z/4133399871716478688/03c93114-c414-4f6a-b443-ad474cb0a30a/1/267a930f-6296-4aae-a6a2-dfd370417a97?authuser > $dep_file
+
+unzip $dep_file
+cd Dependencies
+sudo dpkg -i *.deb
