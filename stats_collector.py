22d20feb68933d02e004ca3885151a6438efb8ea
Matteo Triggiani <davvore33@gmail.com>
2019-11-07 11:50:06 +0100

    Add a new testcase, add a new stats collector

--- stats_collector.py
+++ stats_collector.py
@@ -63,6 +63,8 @@
 import os
 from warp17_api import *
 import socket
+import time
+import os
 
 from rpc_impl import *
 from functools import partial
@@ -76,6 +78,7 @@ from warp17_l3_pb2 import *
 from warp17_app_raw_pb2 import *
 from warp17_app_pb2 import *
 from warp17_server_pb2 import *
+from warp17_app_http_pb2 import *
 from warp17_client_pb2 import *
 from warp17_test_case_pb2 import *
 from warp17_service_pb2 import *
@@ -136,7 +139,7 @@ class Test():
             b2b_port_add_intfs(pcfg,
                                [(Ip(ip_version=IPV4, ip_v4=b2b_ipv4(port, i)),
                                  Ip(ip_version=IPV4, ip_v4=b2b_mask(port, i)),
-                                 b2b_count(port, i)) for i in range(0, n_ip)])
+                                 b2b_count(port, i)) for i in range(0, 1)])
             warp17_call('ConfigurePort', pcfg)
 
         cl_src_ips = b2b_sips(self.cl_port, self.l3_config[self.cl_port][1])
@@ -148,7 +151,7 @@ class Test():
                                     tuc_sports=b2b_ports(self.l4_config[0]),
                                     tuc_dports=b2b_ports(self.l4_config[1])))
 
-        uptime_delay = Delay(d_value=120)
+        uptime_delay = Delay(d_value=2)
         downtime_delay = Delay(d_value=0)
         self.ccfg = TestCase(tc_type=CLIENT, tc_eth_port=self.cl_port,
                              tc_id=self.tc_id,
@@ -177,19 +180,33 @@ class Test():
     def run(self):
         self.add_config()
         self.start()
-        results = self.check_results()
+        status, stats, tstamps = self.check_results(20)
         self.stop()
-        return results
+        return status, stats, tstamps
 
-    def check_results(self):
-        results = {}
+    def check_results(self, times=1):
+        status = []
+        stats = []
+        tstamps = []
         sleep(5)
-        for port in self.l3_config:
-            results[port] = warp17_call('GetTestStatus',
-                                        TestCaseArg(tca_eth_port=port,
-                                                    tca_test_case_id=self.tc_id))
+        while times >= 0:
+            status1 = {}
+            stats1 = {}
+            for port in self.l3_config:
+                status1[port] = warp17_call('GetTestStatus',
+                                           TestCaseArg(tca_eth_port=port,
+                                                       tca_test_case_id=self.tc_id))
+                stats1[port] = warp17_call('GetStatistics',
+                                           TestCaseArg(tca_eth_port=port,
+                                                       tca_test_case_id=self.tc_id))
+                tstamp1 = time.time()
+            status.append(status1)
+            stats.append(stats1)
+            tstamps.append(tstamp1)
+            time.sleep(1)
+            times -= 1
 
-        return results
+        return status, stats, tstamps
 
     def stop(self):
         for port in self.l3_config:
@@ -214,15 +231,16 @@ class Test():
 
 def search_mimimum_memory(pivot, R):
     """Binary search the minimum memory needed to run the test"""
+    localenv = env
     if R < precision:
         return
     print("Running warp17 on {}Mb memory".format(pivot))
     not_started = False
-    env.set_value(env.MEMORY, pivot)
+    localenv.set_value(env.MEMORY, pivot)
 
     try:
         oarg = Warp17OutputArgs(out_file=log_file)
-        proc = warp17_start(env=env, exec_file=bin, output_args=oarg)
+        proc = warp17_start(env=localenv, exec_file=bin, output_args=oarg)
 
     except BaseException as E:
         print("Error occurred: {}".format(E))
@@ -231,31 +249,81 @@ def search_mimimum_memory(pivot, R):
     try:
         warp17_wait(env)
 
-        results = test.run()
+        status, stats, tstamps = test.run()
 
     except BaseException as E:
         print("Error occurred: {}".format(E))
         not_started = True
         pass
 
-    warp17_stop(env, proc)
+    if warp17_stop(env, proc) != 0:
+        os.kill(proc)
+    time.sleep(1)
 
     if not not_started:
-        if test.passed(results):
+        if test.passed(status):
             message = "Success run with {}Mb memory\n".format(pivot)
-            result.write(message)
+            resultwriter.write(message)
             search_mimimum_memory(pivot - R / 2, R / 2)
             return
     message = "Failed run with {}Mb memory\n".format(pivot)
-    result.write(message)
+    resultwriter.write(message)
     search_mimimum_memory(pivot + R / 2, R / 2)
     return
 
+def collect_stats(logwriter, localenv):
+    """Binary search the minimum memory needed to run the test"""
+    print("Running warp17 on {}".format(localenv))
+
+    try:
+        oarg = Warp17OutputArgs(out_file=log_file)
+        # pdb.set_trace()
+        proc = warp17_start(env=localenv, exec_file=bin, output_args=oarg)
+
+    except BaseException as E:
+        print("Error occurred: {}".format(E))
+        exit(-1)
+
+    try:
+        warp17_wait(localenv)
+
+        status, stats, tstamps = test.run()
+
+        warp17_stop(localenv, proc)
+        i = 0
+
+        while i < len(stats):
+            stats1 = stats[i]
+            status1 = status[i]
+            tstamps1 = tstamps[i]
+            message = "timestamp: {} ".format(tstamps1)
+            
+            
+            for port in (test.cl_port, test.sr_port):
+                phystats = stats1[port].sr_phy_rate
+                statusstats = status1[port].tsr_stats
+                link_speed_bytes = float(phystats.pys_link_speed) * 1000 * 1000 / 8
+
+                tx_usage = min(float(phystats.pys_tx_bytes) * 100 / link_speed_bytes, 100.0)
+                rx_usage = min(float(phystats.pys_rx_bytes) * 100 / link_speed_bytes, 100.0)
+                message += "pys_rx_bytes {} ".format(rx_usage)
+                message += "pys_tx_bytes {} ".format(tx_usage)
+                message += "gs_estab {} ".format(statusstats.gs_estab)
+                message += "\n"
+            logwriter.write(message)
+            i += 1
+
+    except BaseException as E:
+        print("Error occurred: {}".format(E))
+        # pass
+
+    return
 
 def test_10m_sessions():
     """Configures a test to run 10 million sessions"""
+    localenv = env
     test_10m = Test()
-    test_10m.add_l3(0, 167837697, 200)  # 10.1.0.1
+    test_10m.add_l3(0, 167837697, 200)  # 10.1.0.1-10.1.0.200
     test_10m.add_l3(1, 167772161, 1)  # 10.0.0.1
     test_10m.l4_config[0] = 50000
     test_10m.l4_config[1] = 1  # not really needed
@@ -263,35 +331,75 @@ def test_10m_sessions():
     test_10m.cl_port = 0
     test_10m.sr_port = 1
 
-    test_10m.cl_test_criteria = TestCriteria(tc_crit_type=CL_ESTAB,
-                                             tc_cl_estab=10000000)
+    test_10m.cl_test_criteria = TestCriteria(tc_crit_type=RUN_TIME,
+                                             tc_cl_estab=120)
 
-    test_10m.app_ccfg = App(app_proto=RAW_CLIENT,
-                            app_raw_client=RawClient(rc_req_plen=10,
-                                                     rc_resp_plen=10))
-    test_10m.app_scfg = App(app_proto=RAW_SERVER,
-                            app_raw_server=RawServer(rs_req_plen=10,
-                                                     rs_resp_plen=10))
+    test_10m.app_ccfg = App(app_proto=HTTP_CLIENT,
+                            app_http_client=HttpClient(hc_req_method=GET,
+                                                       hc_req_object_name='/index.html',
+                                                       hc_req_host_name='www.foobar.net',
+                                                       hc_req_size=10485760))#10MB
+    test_10m.app_scfg = App(app_proto=HTTP_SERVER,
+                            app_http_server=HttpServer(hs_resp_code=OK_200,
+                                                       hs_resp_size=10485760))
 
     start_memory = int(env.get_memory())
 
-    env.set_value(env.TCB_POOL_SZ, 20000)
-    env.set_value(env.UCB_POOL_SZ, 0)
+    localenv.set_value(env.TCB_POOL_SZ, 95000)
+    localenv.set_value(env.UCB_POOL_SZ, 0)
 
     out_folder = "/tmp/10m-test-{}/".format(get_uniq_stamp())
 
-    return test_10m, start_memory, out_folder
+    return test_10m, start_memory, out_folder, localenv
 
+def test_throughoput():
+    """Configures a test to run 10 million sessions"""
+    localenv = env
+    test_thr = Test()
+    test_thr.add_l3(0, 167837697, 1)  # 10.1.0.1
+    test_thr.add_l3(1, 167772161, 10)  # 10.0.0.1-10.0.0.10
+    test_thr.l4_config[0] = 10
+    test_thr.l4_config[1] = 50000  # not really needed
+    test_thr.proto = UDP
+    test_thr.cl_port = 0
+    test_thr.sr_port = 1
 
-test, start_memory, out_folder = test_10m_sessions()  # set your test here
-res_file = "{}res.txt".format(out_folder)
-log_file = "{}out.log".format(out_folder)
-if not os.path.exists(out_folder):
-    os.mkdir(out_folder)
-print "Logs and outputs in " + out_folder
-result = open(res_file, "w")
-result.write("Start binary search {}\n".format(datetime.today()))
-precision = 1000
-search_mimimum_memory(start_memory / 2, start_memory / 2)
-result.write("Finish\n")
-result.close()
+    test_thr.cl_test_criteria = TestCriteria(tc_crit_type=RUN_TIME,
+                                             tc_cl_estab=120)
+
+    test_thr.app_ccfg = App(app_proto=RAW_CLIENT,
+                            app_raw_client=RawClient(rc_req_plen=80000,
+                                                     rc_resp_plen=0))
+    test_thr.app_scfg = App(app_proto=RAW_SERVER,
+                            app_raw_server=RawServer(rs_req_plen=80000,
+                                                     rs_resp_plen=0))
+
+    start_memory = int(env.get_memory())
+
+    localenv.set_value(env.TCB_POOL_SZ, 0)
+    localenv.set_value(env.UCB_POOL_SZ, 95000)
+
+    out_folder = "/tmp/throughoput-test-{}/".format(get_uniq_stamp())
+
+    return test_thr, start_memory, out_folder, localenv
+
+tests = []
+#tests.append(test_throughoput())
+tests.append(test_10m_sessions())
+
+for test, start_memory, out_folder, localenv in tests:
+    # test, start_memory, out_folder = test_throughoput()  # set your test here
+    res_file = "{}res.txt".format(out_folder)
+    log_file = "{}out.log".format(out_folder)
+    if not os.path.exists(out_folder):
+        os.mkdir(out_folder)
+    print "Logs and outputs in " + out_folder
+    resultwriter = open(res_file, "w")
+    resultwriter.write("Start binary search {}\n".format(datetime.today()))
+    for i in range (0, 10):
+        print "run {}".format(i)
+        resultwriter.write("Run {}\n".format(i))
+        collect_stats(resultwriter, localenv)
+    resultwriter.write("Finish\n")
+    resultwriter.close()
+
